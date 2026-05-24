acbd41c6572791b49cc545a551594b4a00a02ba6
leclueleach <lecluesemail@gmail.com>
2026-05-24 13:02:32 +0200

    add user

--- api/create-user.ts
+++ api/create-user.ts
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.VITE_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-)
+    process.env.VITE_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  )
 
 export default async function handler(req: any, res: any) {
   if (req.method !== 'POST') {
